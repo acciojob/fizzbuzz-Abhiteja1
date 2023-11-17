02ceea158cd1a1efe2b26f84b6323f2e8be4a2f0
Dumpala Abhiteja <125918097+Abhiteja1@users.noreply.github.com>
2023-11-17 16:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function fizzBuzz(){
+	for(let i=1 ; i<=20; i++){
+		if(i%3===0 && i%5===0){
+			alert("FizzBuzz");
+		}
+		if(i%3===0){
+			alert("Fizz");
+		}
+		else if(i%5===0){
+			alert("Buzz");
+		}
+		else{
+			alert(i);
+		}
+	}
+}
+fizzBuzz()
